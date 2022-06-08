ee60e670bac50e1b3d506aa1e9878ef2c70bd398
Witcher <[200~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~>
2022-06-08 18:02:00 +0300

    code review

--- src/main/java/ru/zvezdilin/taskOfInterviews/task3_вывод_простых_чисел/Main.java
+++ src/main/java/ru/zvezdilin/taskOfInterviews/task3_вывод_простых_чисел/Main.java
@@ -2,6 +2,7 @@ package ru.zvezdilin.taskOfInterviews.task3_вывод_простых_чисел
 
 public class Main {
     public static void main(String[] args) {
+        //Простое число делится только на 1  и на само себя
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 29, 33};
         int tmp;
         for (int i = 0; i < numbers.length; i++) {
