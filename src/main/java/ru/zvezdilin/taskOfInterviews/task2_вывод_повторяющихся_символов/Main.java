1df7f552a32f9b7ea44a3a6e86c6f83664f81a44
Witcher <[200~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~>
2022-06-08 15:37:33 +0300

    task2

--- src/main/java/ru/zvezdilin/taskOfInterviews/task2_вывод_повторяющихся_символов/Main.java
+++ src/main/java/ru/zvezdilin/taskOfInterviews/task2_вывод_повторяющихся_символов/Main.java
@@ -1,59 +1,43 @@
 package ru.zvezdilin.taskOfInterviews.task2_вывод_повторяющихся_символов;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-/*
-Дана строка String text. Нужно вывести на экран все символы, которые повторяются больше двух раз
- */
 public class Main {
     public static void main(String[] args) {
-        //вариант №1
-        String text = "lHello";
-//        char[] symbols = text.toCharArray();
-//
-//        for (int i = 0; i < symbols.length-1; i++) {
-//            int count = 1;
-//            for (int j = i + 1; j < symbols.length; j++) {
-//                if(Character.toLowerCase(symbols[i])==Character.toLowerCase(symbols[j])) //преобразовние в нижний регистр
-//                    count++;
-//            }
-//            if (count >= 2)
-//                System.out.printf("Символ %s встречается %d раза \n", symbols[i], count);
-//        }
-
-        //вариант №2
-        for (int i = 0; i < text.length() - 1; i++) {
-            int count = 1;
-            for (int j = i + 1; j < text.length(); j++)
-                if (text.charAt(i) == text.charAt(j)) {
-                    count++;
-                }
-            if (count >= 2)
-                System.out.printf("Символ %s повторяется %d раза \n", text.charAt(i), count); // тогда выводит повторяющиеся значения
-        }
-
-        //Вариант №3
-        char[] arraySymbols = text.toCharArray();
-        Set<Character> set = new HashSet<>();
-        for (char arraySymbol : arraySymbols) {
-            set.add(arraySymbol);
-        }
-        for (int i = 0; i < arraySymbols.length; i++) {
-            for (int j = i+1; j < arraySymbols.length; j++) {
-                if((arraySymbols[i]==arraySymbols[j]) && !set.contains(i)){
-                    set.add(arraySymbols[i]);
-                    //Todo добавляем в множество и увеличиваем количество
-
-                }
-             //TODO если в множестве есть символ - меняем значение
-            }
-        }
-        Iterator iterator = set.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
+        String text = "lHelhlo";
+        String[][] arrays = countOfCharInWord(text);
+        printValues(arrays);
     }
 
+    public static String[][] countOfCharInWord(String text) {
+        String[][] arrays = new String[text.length()][2];
+
+        for (int i = 0; i < text.length() - 1; i++) {
+            int count = 1;
+            for (int j = i + 1; j < text.length(); j++) {
+                if (Character.toLowerCase(text.charAt(i)) == Character.toLowerCase(text.charAt(j))) {
+                    count++;
+                }
+                for (int k = 0; k < arrays.length; k++) {
+                    if (arrays[k][0] == String.valueOf(text.charAt(i))) {
+                        continue;
+                    }
+                    else {
+                        arrays[i][0] = String.valueOf(text.charAt(i));
+                        arrays[i][1] = String.valueOf(count);
+                    }
+                }
+            }
+        }
+        return arrays;
+    }
+
+    public static void printValues(String[][] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i][1] != null) {
+                if (Integer.parseInt(arrays[i][1]) >= 2) {
+                    System.out.printf("Символ %s встречается %s раза \n", arrays[i][0], arrays[i][1]);
+                }
+            }
+        }
+    }
 }
+
