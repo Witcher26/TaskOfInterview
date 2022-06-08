1ffb2f0871c4194ddd6df5fc739cb7e733768808
Witcher <[200~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~>
2022-06-08 18:00:42 +0300

    code review

--- src/main/java/ru/zvezdilin/taskOfInterviews/task3_вывод_простых_чисел/Main.java
+++ src/main/java/ru/zvezdilin/taskOfInterviews/task3_вывод_простых_чисел/Main.java
@@ -1,4 +1,17 @@
 package ru.zvezdilin.taskOfInterviews.task3_вывод_простых_чисел;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 29, 33};
+        int tmp;
+        for (int i = 0; i < numbers.length; i++) {
+            tmp = numbers[i];
+            if (tmp % 2 != 0 && tmp % 3 != 0) {
+                System.out.println("Простое число: " + tmp);
+            }
+        }
+    }
 }
+
+
+
