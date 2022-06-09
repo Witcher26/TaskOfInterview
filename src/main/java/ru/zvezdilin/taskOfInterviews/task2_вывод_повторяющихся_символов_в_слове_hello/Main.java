f39a6dd8b3118cb875a4db8c8dde893f3786631b
Witcher <[200~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~>
2022-06-09 10:05:06 +0300

    code review

--- src/main/java/ru/zvezdilin/taskOfInterviews/task2_вывод_повторяющихся_символов_в_слове_hello/Main.java
+++ src/main/java/ru/zvezdilin/taskOfInterviews/task2_вывод_повторяющихся_символов_в_слове_hello/Main.java
@@ -1,36 +1,60 @@
 package ru.zvezdilin.taskOfInterviews.task2_вывод_повторяющихся_символов_в_слове_hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        String text = "Hello";
-        String[][] arrays = countOfCharInWord(text);
-        printValues(arrays);
-    }
+        //Вариант №1
+        String str = "lHello";
+        Map<Character, Integer> storageChars = new HashMap<>();
+        char[] chars = str.toCharArray();
 
-    public static String[][] countOfCharInWord(String text) {
-        String[][] arrays = new String[text.length()][2];
-
-        for (int i = 0; i < text.length() - 1; i++) {
-            int count = 1;
-            for (int j = i + 1; j < text.length(); j++) {
-                if (Character.toLowerCase(text.charAt(i)) == Character.toLowerCase(text.charAt(j))) {
-                    count++;
-                }
-                arrays[i][0] = String.valueOf(text.charAt(i));
-                arrays[i][1] = String.valueOf(count);
-            }
+        for (char aChar : chars) {
+            Integer tmp = storageChars.containsKey(Character.toLowerCase(aChar)) ?
+                    storageChars.put(Character.toLowerCase(aChar), storageChars.get(Character.toLowerCase(aChar)) + 1) :
+                    storageChars.put(Character.toLowerCase(aChar), 1);
         }
-        return arrays;
-    }
 
-    public static void printValues(String[][] arrays) {
-        for (int i = 0; i < arrays.length; i++) {
-            if (arrays[i][1] != null) {
-                if (Integer.parseInt(arrays[i][1]) >= 2) {
-                    System.out.printf("Символ %s встречается %s раза \n", arrays[i][0], arrays[i][1]);
-                }
-            }
+        for (Map.Entry<Character, Integer> mapEntry : storageChars.entrySet()) {
+            Character key = mapEntry.getKey();
+            int value = mapEntry.getValue();
+
+            System.out.println("Символ: \"" + key + "\", количество в слове: \"" + value + "\" раз");
         }
     }
 }
+
+////Вариант №2
+//        String text = "Hello";
+//        String[][] arrays = countOfCharInWord(text);
+//        printValues(arrays);
+//
+//
+//    public static String[][] countOfCharInWord(String text) {
+//        String[][] arrays = new String[text.length()][2];
+//
+//        for (int i = 0; i < text.length() - 1; i++) {
+//            int count = 1;
+//            for (int j = i + 1; j < text.length(); j++) {
+//                if (Character.toLowerCase(text.charAt(i)) == Character.toLowerCase(text.charAt(j))) {
+//                    count++;
+//                }
+//                arrays[i][0] = String.valueOf(text.charAt(i));
+//                arrays[i][1] = String.valueOf(count);
+//            }
+//        }
+//        return arrays;
+//    }
+//
+//    public static void printValues(String[][] arrays) {
+//        for (int i = 0; i < arrays.length; i++) {
+//            if (arrays[i][1] != null) {
+//                if (Integer.parseInt(arrays[i][1]) >= 2) {
+//                    System.out.printf("Символ %s встречается %s раза \n", arrays[i][0], arrays[i][1]);
+//                }
+//            }
+//        }
+//    }
+//}
 
--- src/main/java/ru/zvezdilin/taskOfInterviews/task4_вывод_повторяющихся_слов/Main.java
+++ src/main/java/ru/zvezdilin/taskOfInterviews/task4_вывод_повторяющихся_слов/Main.java
@@ -1,4 +1,24 @@
 package ru.zvezdilin.taskOfInterviews.task4_вывод_повторяющихся_слов;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    //Мама моет раму, Рама держит маму. Вышла замуж мама за индуса Раму и т.д.- `вывести на экран, сколько раз повторяется каждое слово.
+    public static void main(String[] args) {
+        String str = "Мама моет раму, Рама держит маму. Вышла замуж мама за индуса Раму";
+        Map<String, Integer> words = new HashMap<>();
+
+        String[] arr = str.split("\\P{IsAlphabetic}+");
+        for (String s : arr) {
+            Integer anyTmp = words.containsKey(s.toLowerCase()) ? words.put(s.toLowerCase(), words.get(s.toLowerCase()) + 1) : words.put(s.toLowerCase(), 1);
+        }
+
+        for (Map.Entry<String, Integer> mapEntry : words.entrySet()) {
+            String key = mapEntry.getKey();
+            int value = mapEntry.getValue();
+
+            System.out.println("Слово: \"" + key + "\", количество в тексте: \"" + value + "\" раз");
+        }
+    }
 }
